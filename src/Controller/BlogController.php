6d84be8d98ed662cfed7dce2dca1706640e8ba0d
Jérôme LACQUEMANT <jerome.lacquemant@gmail.com>
2020-05-02 17:05:59 +0200

    Add of a button to create a new figure after completion of the form.

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
