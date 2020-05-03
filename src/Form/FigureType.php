2d12dade1d706178b51764387189123435d91cf3
Jérôme LACQUEMANT <jerome.lacquemant@gmail.com>
2020-05-03 20:08:30 +0200

    Modification of the Form to add the category of a figure. It works.

--- src/Form/FigureType.php
+++ src/Form/FigureType.php
@@ -3,8 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Figure;
+use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class FigureType extends AbstractType
@@ -15,6 +17,10 @@ class FigureType extends AbstractType
             ->add('name')
             ->add('content')
             ->add('category')
+            ->add('category_figure', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'title'
+            ])
             ->add('image')
             ->add('video')
         ;
