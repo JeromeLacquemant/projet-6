95d2b5850ed884e6cf62162d48ee662dd05e1888
Jérôme LACQUEMANT <jerome.lacquemant@gmail.com>
2020-05-03 19:41:40 +0200

    Creation of fixtures to have fake datas.

--- src/DataFixtures/FigureFixtures.php
+++ src/DataFixtures/FigureFixtures.php
@@ -2,7 +2,10 @@
 
 namespace App\DataFixtures;
 
+use Faker\Factory;
 use App\Entity\Figure;
+use App\Entity\Comment;
+use App\Entity\Category;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
@@ -10,19 +13,53 @@ class FigureFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        for($i = 1; $i <= 10; $i++)
-        {
-            $figure = new Figure();
-            $figure ->setName("Nom de la figure n°$i")
-                    ->setContent("<p>Description de la figure n°$i</p>")
-                    ->setCategory("Catégorie freestyle")
-                    ->setImage("http://placehold.it/350x150")
-                    ->setVideo("http://placehold.it/350x150")
-                    ->setCreatedAt(new \DateTime());
+        $faker = \Faker\Factory::create('fr_FR');
 
-            $manager->persist($figure);
+        // Creation of 5 faked categories
+        for($i = 1; $i <= 5; $i++) {
+            $category = new Category();
+            $category   ->setTitle($faker->sentence())
+                        ->setDescription($faker->paragraph());
+
+            $manager->persist($category);
+
+            // Creation between 4 and 6 figures for each category
+
+            for($j = 1; $j <= mt_rand(4, 6); $j++) {
+                $figure = new Figure();
+
+                $content ='<p>'.join($faker->paragraphs(5), '</p><p>').'</p>';              $content .= '</p>';
+
+                $figure ->setName($faker->sentence())
+                        ->setContent($content)
+                        ->setCategory("Testtest")
+                        ->setCategoryFigure($category)
+                        ->setImage("http://placehold.it/350x150")
+                        ->setVideo("http://placehold.it/350x150")
+                        ->setCreatedAt($faker->dateTimeBetween('-6 months'));
+    
+                $manager->persist($figure);
+
+                // Providing between 4 and 10 comments for each figure
+                for($k = 1; $k <= mt_rand(4, 10); $k++) {
+                    $comment = new Comment();
+
+                    $content .='<p>'.join($faker->paragraphs(5), '</p><p>').'</p>';  
+
+                    $now = new \DateTime();
+                    $interval = $now->diff($figure->getCreatedAt());
+                    $days = $interval->days;
+                    $minimum = '-' . $days . ' days'; // -100 days
+
+                    $comment    ->setAuthor($faker->name)
+                                ->setContent($content)
+                                ->setCreatedAt($faker->dateTimeBetween($minimum))
+                                ->setFigure($figure);
+
+                    $manager->persist($comment);
+                }
+            }
         }
-
         $manager->flush();
     }
 }
