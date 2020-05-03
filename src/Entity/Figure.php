8b88439c86853936c89a46c1b578ad0634ad9bd2
Jérôme LACQUEMANT <jerome.lacquemant@gmail.com>
2020-05-03 17:02:46 +0200

    Installation of symfony/validator to manage the validations in our foms.

--- src/Entity/Figure.php
+++ src/Entity/Figure.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\FigureRepository")
@@ -18,6 +19,7 @@ class Figure
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Length(min=5, max=255)
      */
     private $name;
 
