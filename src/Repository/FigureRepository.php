7e34be56796567c245250c60fc19b86b28ecb1ef
Jérôme LACQUEMANT <jerome.lacquemant@gmail.com>
2020-05-19 20:33:45 +0200

    Creation of a function findAllPagineEtTrie() in the FigureRepository.

--- src/Repository/FigureRepository.php
+++ src/Repository/FigureRepository.php
@@ -3,8 +3,11 @@
 namespace App\Repository;
 
 use App\Entity\Figure;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use InvalidArgumentException;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
 /**
  * @method Figure|null find($id, $lockMode = null, $lockVersion = null)
@@ -17,6 +20,53 @@ class FigureRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Figure::class);
+    }
+
+    /**
+     * Récupère une liste d'articles triés et paginés.
+     *
+     * @param int $page Le numéro de la page
+     * @param int $nbMaxParPage Nombre maximum d'article par page     
+     *
+     * @throws InvalidArgumentException
+     * @throws NotFoundHttpException
+     *
+     * @return Paginator
+     */
+    
+    public function findAllPagineEtTrie($page, $nbMaxParPage)
+    {
+        if (!is_numeric($page)) {
+            throw new InvalidArgumentException(
+                'La valeur de l\'argument $page est incorrecte (valeur : ' . $page . ').'
+            );
+        }
+
+        if ($page < 1) {
+            throw new NotFoundHttpException('La page demandée n\'existe pas');
+        }
+
+        if (!is_numeric($nbMaxParPage)) {
+            throw new InvalidArgumentException(
+                'La valeur de l\'argument $nbMaxParPage est incorrecte (valeur : ' . $nbMaxParPage . ').'
+            );
+        }
+    
+        $qb = $this->createQueryBuilder('a')
+            ->where('CURRENT_DATE() >= a.datePublication')
+            ->orderBy('a.datePublication', 'DESC');
+        
+        $query = $qb->getQuery();
+
+        $premierResultat = ($page - 1) * $nbMaxParPage;
+        $query->setFirstResult($premierResultat)->setMaxResults($nbMaxParPage);
+        $paginator = new Paginator($query);
+
+        if ( ($paginator->count() <= $premierResultat) && $page != 1) {
+            throw new NotFoundHttpException('La page demandée n\'existe pas.'); // page 404, sauf pour la première page
+        }
+
+        return $paginator;
     }
 
     // /**
