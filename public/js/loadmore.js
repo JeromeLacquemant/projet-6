37a68b37025722f8712b3bcbc936d140a1b2dbf4
Jérôme LACQUEMANT <jerome.lacquemant@gmail.com>
2020-05-31 11:01:32 +0200

    Moving of the JS into a separate folder public/js/ + Modification of base.html.twig to centralise jquery scripts.

---
+++ public/js/loadmore.js
@@ -0,0 +1,13 @@
+
+$("#showMore").click(function(){
+    $("#boxs .box:hidden").slice(0,3).slideDown();
+    if($("#boxs .box:hidden").length == 0){
+        $("#showMore").fadeOut("slow");
+    }
+});
+
+
+
+$(function(){
+    $("img.lazy").lazyload();
+});
