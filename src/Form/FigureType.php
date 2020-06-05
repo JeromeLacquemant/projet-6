8775f62a06b020ffb370b8f3c60662f71e6dd7d4
Jérôme LACQUEMANT <jerome.lacquemant@gmail.com>
2020-06-05 14:50:33 +0200

    Modification of the structure pf public/uploads. Now we have a folder for figures and a folder for users.

--- src/Form/FigureType.php
+++ src/Form/FigureType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Figure;
+use App\Entity\Videos;
 use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
