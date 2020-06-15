d38a976727282c7b73f7f1696dd1eebdaa62b96f
Jérôme LACQUEMANT <jerome.lacquemant@gmail.com>
2020-06-15 16:42:30 +0200

    Now, with the button Add new Image, we obtain the form at the riht place.

--- public/js/image2.js
+++ public/js/image2.js
@@ -1,18 +1,18 @@
 // this variable is the list in the dom, it's initiliazed when the document is ready
-var $collectionHolder;
+var $collectionImage;
 // the link which we click on to add new items
-var $addNewItem = $('<a href="#" class="btn btn-info">Add new item</a>');
+var $addNewImage = $('<a href="#" class="btn btn-info">Add new Image</a>');
 // when the page is loaded and ready
 $(document).ready(function () {
     // get the collectionHolder, initilize the var by getting the list;
-    $collectionHolder = $('#image_list');
+    $collectionImage = $('#image_list');
     // append the add new item link to the collectionHolder
-    $collectionHolder.append($addNewItem);
+    $collectionImage.append($addNewImage);
     // add an index property to the collectionHolder which helps track the count of forms we have in the list
-    $collectionHolder.data('index', $collectionHolder.find('.panel').length)
+    $collectionImage.data('index', $collectionImage.find('.panel').length)
     // finds all the panels in the list and foreach one of them we add a remove button to it
     // add remove button to existing items
-    $collectionHolder.find('.panel').each(function () {
+    $collectionImage.find('.panel').each(function () {
         // $(this) means the current panel that we are at
         // which means we pass the panel to the addRemoveButton function
         // inside the function we create a footer and remove link and append them to the panel
@@ -20,43 +20,43 @@ $(document).ready(function () {
         addRemoveButton($(this));
     });
     // handle the click event for addNewItem
-    $addNewItem.click(function (e) {
+    $addNewImage.click(function (e) {
         // preventDefault() is your  homework if you don't know what it is
         // also look up preventPropagation both are usefull
         e.preventDefault();
         // create a new form and append it to the collectionHolder
         // and by form we mean a new panel which contains the form
-        addNewForm();
+        addNewFormImage();
     })
 });
 /*
  * creates a new form and appends it to the collectionHolder
  */
-function addNewForm() {
+function addNewFormImage() {
     // getting the prototype
     // the prototype is the form itself, plain html
-    var prototype = $collectionHolder.data('prototype');
+    var prototypeImage = $collectionImage.data('prototype');
     // get the index
     // this is the index we set when the document was ready, look above for more info
-    var index = $collectionHolder.data('index');
+    var indexImage = $collectionImage.data('index');
     // create the form
-    var newForm = prototype;
+    var newFormImage = prototypeImage;
     // replace the __name__ string in the html using a regular expression with the index value
-    newForm = newForm.replace(/__name__/g, index);
+    newFormImage = newFormImage.replace(/__name__/g, indexImage);
     // incrementing the index data and setting it again to the collectionHolder
-    $collectionHolder.data('index', index+1);
+    $collectionImage.data('index', indexImage+1);
     // create the panel
     // this is the panel that will be appending to the collectionHolder
-    var $panel = $('<div class="panel panel-warning"><div class="panel-heading"></div></div>');
+    var $panelImage = $('<div class="panel panel-warning"><div class="panel-heading"></div></div>');
     // create the panel-body and append the form to it
-    var $panelBody = $('<div class="panel-body"></div>').append(newForm);
+    var $panelBodyImage = $('<div class="panel-body"></div>').append(newFormImage);
     // append the body to the panel
-    $panel.append($panelBody);
+    $panelImage.append($panelBodyImage);
     // append the removebutton to the new panel
-    addRemoveButton($panel);
+    addRemoveButton($panelImage);
     // append the panel to the addNewItem
     // we are doing it this way to that the link is always at the bottom of the collectionHolder
-    $addNewItem.before($panel);
+    $addNewImage.before($panelImage);
 }
 
 /**
