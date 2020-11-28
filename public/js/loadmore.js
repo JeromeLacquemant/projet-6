9bb814bf466d373fde40f1ce2cd2e94a349c5e3c
Jérôme LACQUEMANT <jerome.lacquemant@gmail.com>
2020-11-28 11:22:26 +0100

    Addig a button + javascript to load photos and videos

--- public/js/loadmore.js
+++ public/js/loadmore.js
@@ -1,4 +1,5 @@
 
+// Loading of comments for the page of one article and figures on the home page
 $("#showMore").click(function(){
     $("#boxs .box:hidden").slice(0,3).slideDown();
     if($("#boxs .box:hidden").length === 0){
@@ -9,3 +10,15 @@ $("#showMore").click(function(){
 $(function(){
     $("img.lazy").lazyload();
 });
+
+
+// Loading of photos and videos for a page of a figure
+var buttonFiles = document.getElementById("showMoreFile") ;
+var files = document.getElementById("files") ;
+
+files.style.display = "none";
+
+buttonFiles.addEventListener("click", () => {
+    $("#showMoreFile").fadeOut("slow");
+    files.style.display = "block";
+}) ;
